2598e1abe8dce1169806755833b0f78d6ed3b57b
Dowdle <thedowdle@byui.edu>
2018-05-09 15:07:25 -0600

    Breadcrumb trail shows up on home page and other pages

--- scripts/breadcrumbs.js
+++ scripts/breadcrumbs.js
@@ -5,6 +5,7 @@
 // Get the sections of current course
 function getData(courseID) {
     return new Promise((resolve, reject) => {
+        // let courseID = document.location.href.split("/")[4];
         function receive() {
             var data = JSON.parse(this.responseText.replace('while(1);', ''));
             resolve(data);
@@ -23,40 +24,46 @@ function getData(courseID) {
 }
 
 
-function createSectionNumber(course) {
-    course.forEach(course => {
-        var cellContents = '';
-        console.log(course);
-
-        //===============================================
-        // What is section doing?
-        // ==============================================
-        cellContents = course.section.map(section => {
-            var colorClass = section.nonxlist_course_id !== null ? 'color:#42aaf4' : '';
-            var sectionName = section.name.replace(/section/i, '').replace(/\s*/, '');
-            // Reference courses to REF
-            sectionName = sectionName.includes('Reference') ? 'REF' : sectionName;
-            // Course Council View courses to CCV
-            sectionName = sectionName.includes('Course Council View') ? 'CCV' : sectionName;
-            // Blueprint courses to BP
-            sectionName = sectionName.includes('Blueprint') ? 'BP' : sectionName;
-            // Long section names to just 3 letters with ...
-            sectionName = sectionName.length > 5 ? sectionName.substring(0, 5) + '...' : sectionName;
-            // Pad single numbers with a 0
-            if (sectionName.length === 1) {
-                sectionName = '0' + sectionName;
-            }
-            return `<span style="${colorClass}">${sectionName}</span>`;
-        }).join(', ');
-    });
+function createSectionNumbers(sections) {
+    console.log('SECTION', sections);
+    var sectionNumbers = sections.map(section => {
+        var colorClass = section.nonxlist_course_id !== null ? 'color:#42aaf4' : '';
+        var sectionName = section.name.replace(/section/i, '').replace(/\s*/, '');
+        // Reference courses to REF
+        sectionName = sectionName.includes('Reference') ? 'REF' : sectionName;
+        // Course Council View courses to CCV
+        sectionName = sectionName.includes('Course Council View') ? 'CCV' : sectionName;
+        // Blueprint courses to BP
+        sectionName = sectionName.includes('Blueprint') ? 'BP' : sectionName;
+        // Long section names to just 3 letters with ...
+        sectionName = sectionName.length > 5 ? sectionName.substring(0, 5) + '...' : sectionName;
+        // Pad single numbers with a 0
+        if (sectionName.length === 1) {
+            sectionName = '0' + sectionName;
+        }
+        return `<span style="${colorClass}">${sectionName}</span>`;
+    }).join(', ');
+    return `(${sectionNumbers})`;
 }
 
 
 function main() {
-    let courseID = document.location.href.split("/")[4];
 
-    let course = getData(courseID)
-        .then(createSectionNumber)
-        .then(console.log);
+    let courseID = document.location.href.split("/")[4];
+    let div = document.querySelector(`#breadcrumbs a[href*='/courses/${courseID}']`);
+    console.log(div);
+
+    getData(courseID)
+        .then(createSectionNumbers)
+        .then(sectionNumbers => {
+            console.log(sectionNumbers);
+            if(sectionNumbers !== '()'){
+            div.innerHTML += " " + sectionNumbers;
+        }
+        })
+
+        .catch(console.error);
+    
 }
+
 main()
