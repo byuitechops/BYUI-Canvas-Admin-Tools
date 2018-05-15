9a36578111380539d647c6bf416a40135566b8d2
Theron Dowdle <therondowdle7@gmail.com>
2018-05-15 16:39:51 -0600

    Lock blueprint courses.

---
+++ scripts/lock.js
@@ -0,0 +1,28 @@
+console.log("Running script");
+
+let button = document.getElementById('lockSections');
+button.addEventListener("click", lockElements);
+
+function lockElements() {
+    let elms = document.querySelectorAll('.icon-blueprint');
+    if (elms.length === 0) {
+        console.log("There is nothing to lock.");
+        return;
+    }
+    elms.forEach(el => {
+        el.click();
+    })
+    console.log('Locked');
+}
+
+function unlockElements() {
+    let elms = document.querySelectorAll('.icon-blueprint-lock');
+    if (elms.length === 0) {
+        console.log("There is nothing to unlock.");
+        return;
+    }
+    elms.forEach(el => {
+        el.click();
+    })
+    console.log('Unlocked');
+}
