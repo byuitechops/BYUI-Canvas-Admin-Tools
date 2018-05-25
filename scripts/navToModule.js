813cf589ffdc79137281b3ec197e537d332f9c4f
Dowdle <thedowdle@byui.edu>
2018-05-25 15:49:14 -0600

    Found all modules and module names. Only need to format/event listeners

--- scripts/navToModule.js
+++ scripts/navToModule.js
@@ -19,24 +19,37 @@ function getModules() {
     anchorTags = Array.from(anchorTags).filter(anchorTag => {
         return anchorTag.id !== 'module_';
     });
-    console.log(anchorTags);
+    return anchorTags;
 }
 
 function createNavbar() {
     let navbar = document.createElement('div');
+    // Set up the element however you need: styling, sizing, etc.
     navbar.id = 'potato';
     navbar.style.height = '5vh';
     navbar.style.width = '50vw';
+    navbar.style.zIndex = 10;
+    navbar.style.backgroundColor = 'black';
+    navbar.style.color = 'white';
     navbar.style.position = 'fixed';
     navbar.style.bottom = 0;
     navbar.style.right = 0;
-    // Set up the element however you need: styling, sizing, etc.
+    navbar.style.display = 'flex';
+
     document.querySelector('body').appendChild(navbar);
+
 }
 
-function setNavItems() {
 
+function fillNavbar(modules) {
+    let format = '';
+    modules.forEach(module => {
+        let title = document.querySelector(`#${module.id} + div>h2`).innerHTML;
+        format += `<span style="margin:0 10px;">${title}</span>`;
+    })
+    document.querySelector('#potato').innerHTML = format;
 }
 
 getModules();
 createNavbar();
+fillNavbar(getModules());
