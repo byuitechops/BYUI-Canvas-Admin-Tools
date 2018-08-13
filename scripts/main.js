3ee9409d793f23710442e9545ec2a53c4ea48a93
Colter-Hammer <therondowdle7@gmail.com>
2018-08-13 16:36:38 -0600

    If option is selected, add to popup started

--- scripts/main.js
+++ scripts/main.js
@@ -12,14 +12,20 @@ function setItem(event) {
     });
 }
 
+function addDeleteQuizQuestions() {
+    let quizPopup = `<label id="quizQuestionsDelete" class="container" for="deleteQuestions">Delete Quiz Questions
+        <button type="button" id="deleteQuizQuestions">Delete Questions</button>
+    </label>`
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    // chrome.storage.sync.get({
-    //     displaySections: 'potato'
-    // }, (items) => {
-    //     Object.keys(items).forEach(key => {
-    //         document.querySelector(`#${key}`).checked = items[key];
-    //     });
-    // });
+    chrome.storage.sync.get({
+        displaySections: 'potato'
+    }, (items) => {
+        Object.keys(items).forEach(key => {
+            document.querySelector(`#${key}`).checked = items[key];
+        });
+    });
 
     document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
         checkbox.addEventListener('click', setItem);
@@ -33,5 +39,15 @@ document.addEventListener('DOMContentLoaded', () => {
         lockItems(false);
     });
 
-    document.querySelector('#deleteQuizQuestions').addEventListener('click', quizQuestions);
+    document.querySelector('#deleteQuizQuestions').length > 1 ? document.querySelector('#deleteQuizQuestions').addEventListener('click', quizQuestions) : '';
+});
+
+
+/* If the option to show the cross-listed column is on, then do it */
+chrome.storage.sync.get({
+    deleteQuizQuestions: false,
+}, function (items) {
+    if (items.deleteQuizQuestions === true) {
+        addDeleteQuizQuestions();
+    }
 });
