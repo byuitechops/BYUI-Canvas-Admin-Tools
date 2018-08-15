c6d3cbb69b08449de6752e482af76b433989fce4
Colter-Hammer <therondowdle7@gmail.com>
2018-08-15 15:17:15 -0600

    Made some progress on the code. Now successfully loads full page, gets div, and clicks edit button, but takes much too long.

--- scripts/editQuestionBanks.js
+++ scripts/editQuestionBanks.js
@@ -9,20 +9,60 @@
  * 
  */
 
-function EditButton() {
-	let editButtons = document.querySelectorAll("#questions .edit_question_link");
-	editButtons.forEach(el => el.click());
-	return;
+function loadFullPage() {
+	return new Promise((resolve, reject) => {
+		let more_questions = document.querySelector('#more_questions')
+		if (more_questions === null) { return resolve(false); }
+
+		let totalPages = more_questions.dataset.totalPages;
+		let currPage = more_questions.dataset.currentPage;
+		let next_page = document.querySelector('#more_questions .more_questions_link');
+		function openPages() {
+			next_page.click();
+			currPage = more_questions.dataset.currentPage;
+
+			if (currPage < totalPages) {
+				setTimeout(openPages, 100);
+			} else {
+				resolve(true);
+			}
+		}
+		openPages();
+	})
 }
 
-function getClass() {
-
-	/**
-	 * Check for '.more_questions_link'
-	 * if it exists, go with '.edit_teaser_link'
-	 * if not '.edit_question_link'
-	 */
+function getClass(check) {
+	let classCheck = check ? '.edit_teaser_link' : '.edit_question_link';
+	console.log(classCheck);
+	return classCheck;
 }
+
+function editButton(editClass) {
+	return new Promise((resolve, reject) => {
+		let editButtons = document.querySelectorAll(`#questions ${editClass}`);
+		editButtons.forEach(el => el.click());
+		resolve();
+	})
+}
+
+function main(check) {
+	return new Promise((resolve, reject) => {
+		let buttonClass = getClass(check);
+		editButton(buttonClass)
+			.then(resolve('Finished'));
+		console.log(buttonClass);
+	})
+}
+
+chrome.runtime.onMessage.addListener(
+	function (request, sender, sendResponse) {
+		if (request.call === 'divsToQuestionBanks') {
+			loadFullPage()
+				.then(main)
+				.then(console.log)
+		}
+		sendResponse()
+	});
 
 
 
@@ -30,54 +70,44 @@ function getClass() {
 // #1 EDIT
 
 
-setTimeout(function () {
-	setTimeout(function () {
-		// #2 HTML
-		console.log("Switching to html mode...");
-		// Changes all questions to HTML editing mode
-		var htmlButtons = document.querySelectorAll(".toggle_question_content_views_link");
-		var i;
-		for (i = 0; i < htmlButtons.length - 2; i++) {
-			if (htmlButtons[i].innerHTML == "HTML Editor") {
-				htmlButtons[i].click();
-			};
-		};
+// setTimeout(function () {
+// 	setTimeout(function () {
+// 		// #2 HTML
+// 		console.log("Switching to html mode...");
+// 		// Changes all questions to HTML editing mode
+// 		var htmlButtons = document.querySelectorAll(".toggle_question_content_views_link");
+// 		var i;
+// 		for (i = 0; i < htmlButtons.length - 2; i++) {
+// 			if (htmlButtons[i].innerHTML == "HTML Editor") {
+// 				htmlButtons[i].click();
+// 			};
+// 		};
 
-		setTimeout(function () {
-			// #3 DIVS
-			console.log("Adding divs...");
-			// Adds proper divs to each question
-			var textareas = document.querySelectorAll(".question_content");
-			var i;
-			for (i = 0; i < textareas.length - 1; i++) {
+// 		setTimeout(function () {
+// 			// #3 DIVS
+// 			console.log("Adding divs...");
+// 			// Adds proper divs to each question
+// 			var textareas = document.querySelectorAll(".question_content");
+// 			var i;
+// 			for (i = 0; i < textareas.length - 1; i++) {
 
-				// >---------------------** CHANGE THE CLASS CODE HERE **---------------------< 
+// 				// >---------------------** CHANGE THE CLASS CODE HERE **---------------------< 
 
-				if (textareas[i].value.includes("<div class=\"byui cit365\">") != true) {
-					textareas[i].value = "<div class=\"byui cit365\">" + textareas[i].value + "</div>";
-				}
-			};
+// 				if (textareas[i].value.includes("<div class=\"byui cit365\">") != true) {
+// 					textareas[i].value = "<div class=\"byui cit365\">" + textareas[i].value + "</div>";
+// 				}
+// 			};
 
-			setTimeout(function () {
-				// #4 SAVE
-				console.log("Updating questions...");
-				// Saves/updates all the questions
-				var updateButtons = document.querySelectorAll(".btn.btn-small.submit_button.btn-primary");
-				var i;
-				for (i = 0; i < updateButtons.length - 1; i++) {
-					updateButtons[i].click();
-				};
-			}, 3000);
-		}, 3000);
-	}, 3000);
-}, 3000);
-
-chrome.runtime.onMessage.addListener(
-	function (request, sender, sendResponse) {
-		if (request.call === 'deleteQuestions') {
-			var elt = document.createElement("script");
-			elt.innerHTML = '(' + deleteQuestions.toString() + ')()';
-			document.head.appendChild(elt);
-		}
-		sendResponse()
-	});
+// 			setTimeout(function () {
+// 				// #4 SAVE
+// 				console.log("Updating questions...");
+// 				// Saves/updates all the questions
+// 				var updateButtons = document.querySelectorAll(".btn.btn-small.submit_button.btn-primary");
+// 				var i;
+// 				for (i = 0; i < updateButtons.length - 1; i++) {
+// 					updateButtons[i].click();
+// 				};
+// 			}, 3000);
+// 		}, 3000);
+// 	}, 3000);
+// }, 3000);
