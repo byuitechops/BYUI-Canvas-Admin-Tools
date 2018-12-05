b0aa3f44a7f39727892d1991e801f7350d23aff9
Stum <levistum@byui.edu>
2018-12-05 15:19:59 -0700

    Trying to display select button but it dumb

---
+++ scripts/selectAllFiles.js
@@ -0,0 +1,17 @@
+function displaySelectAllButton() {
+    // let checkBox = document.querySelector('#selectAllCheckbox');
+    // let label = document.querySelector('#selectAllCheckbox+[for=selectAllCheckbox]');
+
+    // checkBox.classList.remove("screenreader-only");
+    // label.classList.remove("screenreader-only");
+    console.log("Running script");
+}
+
+/* If the option to show the cross-listed column is on, then do it */
+chrome.storage.sync.get({
+    selectAllFiles: false,
+}, function (items) {
+    if (items.selectAllFiles === true) {
+        displaySelectAllButton();
+    }
+});
