a353397a76d4ebbcc7985afd4f8c91c4fa84c407
Stum <levistum@byui.edu>
2018-12-06 12:18:51 -0700

    Its working!

--- scripts/selectAllFiles.js
+++ scripts/selectAllFiles.js
@@ -1,13 +1,42 @@
-function displaySelectAllButton() {
-    let checkBox = document.querySelector('#selectAllCheckbox');
-    let label = document.querySelector('#selectAllCheckbox+[for=selectAllCheckbox]');
-
-    checkBox.classList.remove("screenreader-only");
-    label.classList.remove("screenreader-only");
-    console.log("Running script");
+/**
+ * Waits for an event to happen within the DOM before calling a callback function
+ * 
+ * @param {string} parent The element you want to watch for an event
+ * @param {function} fn The event you are waiting for
+ * @param {function} cb What to do when the event happens
+ */
+function waitFor(parent, fn, cb) {
+    var observer = new MutationObserver(() => {
+        if (fn()) {
+            observer.disconnect();
+            cb();
+        }
+    });
+    observer.observe(parent, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+    });
 }
 
-/* If the option to show the cross-listed column is on, then do it */
+/**
+ * waits for the document to load.
+ * Then, it shows the "select all" button
+ */
+function displaySelectAllButton() {
+    var checkbox, label;
+    waitFor(document, () => {
+        checkbox = document.querySelector('#selectAllCheckbox');
+        label = document.querySelector('#selectAllCheckbox+[for=selectAllCheckbox]');
+        return checkbox && label;
+    }, () => {
+        checkbox.classList.remove("screenreader-only");
+        label.classList.remove("screenreader-only");
+        console.log("Running script");
+    });
+}
+
+/* If the option to show the select-all button is on, then do it */
 chrome.storage.sync.get({
     selectAllFiles: false,
 }, function (items) {
