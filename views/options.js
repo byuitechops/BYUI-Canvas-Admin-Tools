e5f4ad5f2665a3669e905ae2a1727a39ba8daccc
Colter-Hammer <therondowdle7@gmail.com>
2019-01-24 13:40:34 -0700

    Simplified adding features to the extension through JSON

--- views/options.js
+++ views/options.js
@@ -1,80 +1,42 @@
-// An array of all of the features added
-// This should be the name that is used everywhere, so all the loops work
-const features = [
-    {
-        id: 'sectionsColumn',
-        title: 'Display Sections Column',
-        description: 'This will display a new column listing the sections for each course on the right side of the search results.If a section was cross - listed into the course, it is highlighted in blue.Blueprint courses display as "BP", Reference courses as "REF", and courses that do not have sections but have a long title, are shortened to five letters.',
-        type: 'display'
-    },
-    {
-        id: 'sectionsBreadcrumb',
-        title: 'Display Sections in Breadcrumb',
-        description: 'This will display the sections for the course in the breadcrumb after the course name. If a section was cross-listed into the course, it is highlighted in blue.Blueprint courses display as "BP", Reference courses as "REF", and courses that do not have sections but have a long title, are shortened to five letters.',
-        type: 'display'
-    },
-    {
-        id: 'navToModules',
-        title: 'Show Module Navbar',
-        description: 'This will display the navbar on the modules page. All of the week/lesson modules have been shortened to \'W\' followed by the week/lesson.',
-        type: 'display'
-    },
-    {
-        id: 'addBlueprintParent',
-        title: 'Link to Blueprint Parent Account',
-        description: 'This will add a link to the modules navbar at the bottom of the page that links to the parent blueprint.If there is no blueprint for the course nothing will show up there. Operates seperate of the module navbar, so it will show up with or without the navbar option being selected.',
-        type: 'display'
-    },
-    {
-        id: 'deleteQuizQuestions',
-        title: 'Delete all Quiz Questions',
-        description: 'Show the option to delete all quiz questions in the extension popup. When you click the button it will confirm if you want to delete <em>All</em> of the quiz questions.It will not confirm for each individual question.',
-        type: 'popup'
-    },
-    {
-        id: 'blueprintLockItems',
-        title: 'Lock/Unlock blueprint items',
-        description: 'Show the option to lock and unlock blueprint items in the extension popup.',
-        type: 'popup'
-    },
-    {
-        id: 'divsToQuestions',
-        title: 'Add Divs to Quiz Questions',
-        description: 'Show the option to add Divs on quiz questions in the extension popup. This feature takes a little bit to run, but it\'s a lot faster than doing them individually.If you\'re looking at the console, you will see a lot of errors, but those are because the tool is running too fast for the page to load completely.',
-        type: 'popup'
-    },
-    {
-        id: 'adminAccountNames',
-        title: 'Display Sub-accounts',
-        description: 'This option will show the sub-account of courses in the Admin view. Will only display a sub-account if it\'s different than the previously displayed value.',
-        type: 'display'
-    },
-    {
-        id: 'blueprintAssociations',
-        title: 'Link Associated Blueprint Courses',
-        description: 'When you go to the associated courses tab in a blueprint course, this will make each course title a link to the correct course.',
-        type: 'display'
-    },
-    {
-        id: 'selectAllFiles',
-        title: 'Select All Files Button',
-        description: 'Displays the "select all" button when working with files.',
-        type: 'display'
-    },
-    {
-        id: 'killQuizzes',
-        title: 'Deletes All Quizzes',
-        description: 'Deletes all the quizzes in the Quizzes section.',
-        type: 'popup'
-    }
-];
+function getJson() {
+    return new Promise((resolve, reject) => {
+        let extId = chrome.runtime.id;
 
-createPage(features);
+        function receive() {
+            let stuff = this.response;
+            resolve(stuff);
+        }
 
+        function onError(err) {
+            console.error(err);
+        }
 
-/**
+        var request = new XMLHttpRequest();
+        request.addEventListener('load', receive);
+        request.addEventListener('error', onError);
+        request.open('GET', `chrome-extension://${extId}/options.json`);
+        request.send();
+    });
+}
+
+getJson().then(el => {
+    let features = JSON.parse(el).details;
+    createPage(features);
+    return features;
+}).then(features => {
+    getOptions(features);
+    document.querySelectorAll('.switch').forEach(el => el.addEventListener('click', colorItem));
+    document.querySelectorAll('.switch').forEach(el => el.addEventListener('click', () => {
+        saveOptions(features);
+    }));
+});
+
+/*******************************
+ * 
+ * 
  * Change the color of the checkbox
- */
+ * 
+********************************/
 function colorItem(event) {
     let el;
     if (event.target) {
@@ -92,28 +54,31 @@ function colorItem(event) {
     }
 }
 
-/**
+
+/********************************
+ * 
+ * 
  * Saves options to chrome.storage
- */
-function saveOptions() {
+ * 
+********************************/
+function saveOptions(features) {
     let checkedFeatures = features.reduce((acc, curr) => {
         acc[curr.id] = document.querySelector(`#${curr.id}`).checked;
         return acc;
     }, {});
 
     chrome.storage.sync.set(
-        checkedFeatures,
-        () => {
-            console.log('Options saved');
-        }
-    );
+        checkedFeatures);
 }
 
-/**
+
+/********************************
+ *
  * Gets options from chrome.storage
  * Null value on the "get" returns all of the items
- */
-function getOptions() {
+ *
+ ********************************/
+function getOptions(features) {
     chrome.storage.sync.get(null, items => {
         features.forEach(curr => {
             document.querySelector(`#${curr.id}`).checked = items[curr.id];
@@ -122,18 +87,26 @@ function getOptions() {
     });
 }
 
-// Check if the extension was just installed
+/********************************
+ *
+ * Check to see if the extension
+ * was just downloaded or updated.
+ * If one of those things is true,
+ * it will add some stuff to the
+ * page to help with navigation. 
+ *
+ ********************************/
 let bg = chrome.extension.getBackgroundPage().firstLoad();
-if (bg <= 1) {
+if (bg === 'installed') {
     console.log('First Load');
     firstOpen();
-    document.querySelector('body').style.backgroundColor = 'pink';
-} else {
+} else if (bg === 'updated') {
+    updated();
     console.log('Not the first load');
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    getOptions();
-    document.querySelectorAll('.switch').forEach(el => el.addEventListener('click', colorItem));
-    document.querySelectorAll('.switch').forEach(el => el.addEventListener('click', saveOptions));
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     getOptions(features);
+//     document.querySelectorAll('.switch').forEach(el => el.addEventListener('click', colorItem));
+//     document.querySelectorAll('.switch').forEach(el => el.addEventListener('click', saveOptions));
+// });
