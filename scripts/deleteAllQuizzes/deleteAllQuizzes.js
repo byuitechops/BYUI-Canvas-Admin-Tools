04eb1fb839ff88cec01da4f0e0095dc248ad0c76
Colter-Hammer <therondowdle7@gmail.com>
2019-04-03 17:46:59 -0600

    comment unused function. Didn't delete it because it could possibly be useful in the future

--- scripts/deleteAllQuizzes/deleteAllQuizzes.js
+++ scripts/deleteAllQuizzes/deleteAllQuizzes.js
@@ -3,22 +3,23 @@
  * Adds delete button to options above quizzes.
  * We have added the functionality to the chrome extension popup instead.
  */
-function addDeleteButton() {
-    var toolbar = document.querySelector(".header-bar-right #toolbar-1");
-    var deleteOption = document.createElement("li");
-    deleteOption.classList.add("ui-menu-item");
+// function addDeleteButton() {
+//     var toolbar = document.querySelector(".header-bar-right #toolbar-1");
+//     var deleteOption = document.createElement("li");
+//     deleteOption.classList.add("ui-menu-item");
 
-    var deleteButton = document.createElement("a");
-    deleteButton.id = "killQuizzes";
-    deleteButton.classList.add("icon-trash", "ui-corner-all");
-    deleteButton.innerHTML = "Delete All Quizzes";
+//     var deleteButton = document.createElement("a");
+//     deleteButton.id = "killQuizzes";
+//     deleteButton.classList.add("icon-trash", "ui-corner-all");
+//     deleteButton.innerHTML = "Delete All Quizzes";
 
-    deleteOption.appendChild(deleteButton);
-    toolbar.appendChild(deleteOption);
-}
+//     deleteOption.appendChild(deleteButton);
+//     toolbar.appendChild(deleteOption);
+// }
 
 /**
  * Alerts the user that they're about to delete all the quizzes.
+ * Deletes all the quizzes
  */
 function deleteAllQuizzes() {
     var realConfirm = window.confirm;
